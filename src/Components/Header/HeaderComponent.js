375a0d5abf197d5d104d10a537e54f583f474bc1
Fabien-Junior <fabienjunior.fresse@gmail.com>
2021-03-14 18:23:04 +0100

    Adds a language button in the header

--- src/Components/Header/HeaderComponent.js
+++ src/Components/Header/HeaderComponent.js
@@ -2,9 +2,10 @@
 import React, { Component } from "react";
 
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import { Toolbar, Button } from "@material-ui/core";
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { Fade } from '@material-ui/core';
+import { Translate } from "react-redux-i18n";
 
 function ShowOnScroll(props) {
   const { children } = props;
@@ -17,15 +18,21 @@ function ShowOnScroll(props) {
   );
 }
 
-class HeaderComponent extends Component {
+class HeaderComponent extends Component<{
+  locale: string,
+  setLocale: (lang: string) => void
+}> {
+
+  handleClick = () => {
+    this.props.setLocale(this.props.locale == "fr" ? "en" : "fr");
+  }
+
   render() {
     return (
       <ShowOnScroll>
         <AppBar color="default">
           <Toolbar variant="dense" className="flexJustifyBetween">
-            <div>
-              {this.props.location ? this.props.location.pathname.split("/")[this.props.location.pathname.split("/").length - 1] : null}
-            </div>
+            <Button variant="outlined" size="small" style={{ position: "absolute", right: "1em", color: "white", borderColor: "white" }} onClick={this.handleClick}><Translate value="switchLang" /></Button>
           </Toolbar>
         </AppBar>
       </ShowOnScroll>
--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,9 +1,15 @@
 import { connect } from "react-redux";
 import HeaderComponent from "../Components/Header/HeaderComponent";
+import { setLocale } from "react-redux-i18n";
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: State) => ({
+  locale: state.i18n.locale
 });
 
-export default connect(
-  mapStateToProps
-)(HeaderComponent);
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setLocale: (lang: string) => {
+    dispatch(setLocale(lang));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderComponent);
